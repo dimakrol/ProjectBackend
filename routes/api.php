fa479f13189219175d6cded96370b69fc953b077
dimakrol <dima.krol@mail.ru>
2017-10-11 01:44:47 +0300

    Implemented JWT authentication.

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::post('users', 'UsersController@signup');
+Route::post('users/signin', 'UsersController@signin');
